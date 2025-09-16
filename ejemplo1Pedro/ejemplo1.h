02bf2e6420be5b52a80a0ffc408519795a2968b4
usuario <p.casadomorcillo@gmail.com>
2025-09-16 17:41:12 +0200

    0

---
+++ ejemplo1Pedro/ejemplo1.h
@@ -0,0 +1,21 @@
+#ifndef ejemplo1_H
+#define ejemplo1_H
+
+#include <QtGui>
+#include "ui_counterDlg.h"
+
+class ejemplo1 : public QWidget, public Ui_Counter
+{
+    Q_OBJECT
+    public:
+        ejemplo1();
+
+    public slots:
+        void doButton();
+        void doCount();
+
+    private:
+        QTimer timer;
+};
+
+#endif // ejemplo1_H
