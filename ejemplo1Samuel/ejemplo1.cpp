29de9163ad157b26aab0bc7325ebc55a5955f035
Samuel Mena <s.a.m.u.e.l.m.e.n.a.f.e.r@gmail.com>
2025-09-27 01:27:42 +0200

    ejemplo Samuel con el programa de la tarea 0 hecho junto Timer en .h

--- ejemplo1Samuel/ejemplo1.cpp
+++ ejemplo1Samuel/ejemplo1.cpp
@@ -5,20 +5,53 @@ ejemplo1::ejemplo1(): Ui_Counter()
 {
 	setupUi(this);
 	show();
-	connect(button, SIGNAL(clicked()), this, SLOT(doButton()) );
+	count = 0;
+    sense = true;
+	connect(stop, SIGNAL(clicked()), this, SLOT(doStop()) );
+	connect(reset, SIGNAL(clicked()), this, SLOT(doReset()) );
+    connect(reverse, SIGNAL(clicked()), this, SLOT(changeSense()) );
 	connect(&timer, SIGNAL(timeout()), this, SLOT(doCount()) );
+	connect(slider, SIGNAL(valueChanged(int)), this, SLOT(doPeriod()) );
+    connect(slider, SIGNAL(valueChanged(int)), this, SLOT(updateValueSliderLabel(int)) );
 	timer.start(500);
 }
 
-void ejemplo1::doButton()
+void ejemplo1::doStop()
 {
-	qDebug() << "click on button";
+	if (timer.isActive()) {
+		timer.stop();
+	} else {
+		timer.start(500);
+	}
 }
 
 void ejemplo1::doCount()
 {
-	static int count = 0;
-	lcdNumber->display(count++);
+    if(sense)
+        lcdNumber->display(count++);
+    else
+        lcdNumber->display(count--);
+}
+
+void ejemplo1::doReset()
+{
+	count = 0;
+	lcdNumber->display(count);
+}
+
+void ejemplo1::doPeriod()
+{
+    timer.setInterval(slider->maximum() - slider->value());
+}
+
+void ejemplo1::updateValueSliderLabel(int value)
+{
+    label->setText(QString::number(value) + "m/s");
+}
+
+void ejemplo1::changeSense()
+{
+    sense = !sense;
 }
 
 
--- ejemplo1Samuel/ejemplo1.h
+++ ejemplo1Samuel/ejemplo1.h
@@ -11,11 +11,67 @@ class ejemplo1 : public QWidget, public Ui_Counter
         ejemplo1();
 
     public slots:
-        void doButton();
+        void doStop();
         void doCount();
+        void doReset();
+        void doPeriod();
+        void updateValueSliderLabel(int value);
+        void changeSense();
 
     private:
         QTimer timer;
+        int count;
+        bool sense;
 };
 
 #endif // ejemplo1_H
+
+/*
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <thread>
+#include <chrono>
+#include <functional>
+#include <future>
+#include <cstdio>
+#include <iostream>
+
+class Timer
+{
+    public:
+
+        Timer(){};
+
+        template <class callable>
+        void connect(callable&& f)
+        {
+			std::thread([=]()
+            {
+                while(true)
+                {
+					if(go.load())
+						std::invoke(f);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(period.load()));
+                }
+            }).detach();
+        };
+
+        void start(int p)
+        {
+			period.store(p);
+			go.store(true);
+        };
+
+        void stop() { go.store(!go); };
+		void setPeriod(int p) { period.store(p) ;};
+
+    private:
+        std::atomic_bool go = false;
+		std::atomic_int period = 0;
+
+
+};
+
+#endif // TIMER_H
+*/
