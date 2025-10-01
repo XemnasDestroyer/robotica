af9c1c9835284727b0deaf2d7d6f69c5c8ed06b8
Samuel Mena <147733913+SamuMoga@users.noreply.github.com>
2025-10-01 20:59:06 +0200

    Delete chocachoca directory

--- chocachoca/generated/CMakeFiles/ICES__home_samumoga_robocomp_components_chocachoca_generated.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for ICES__home_samumoga_robocomp_components_chocachoca_generated.
--- chocachoca/generated/CMakeFiles/ICE_GenericBase_target.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for ICE_GenericBase_target.
-# This may be replaced when dependencies are built.
