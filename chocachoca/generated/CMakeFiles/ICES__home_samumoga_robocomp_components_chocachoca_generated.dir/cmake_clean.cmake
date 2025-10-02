5fb5649544d9bba402b980b9346875f4f6e2c825
Samuel Mena <s.a.m.u.e.l.m.e.n.a.f.e.r@gmail.com>
2025-10-02 20:46:08 +0200

    Agregar carpeta chocachoca con configs y src

---
+++ chocachoca/generated/CMakeFiles/ICES__home_samumoga_robocomp_components_chocachoca_generated.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICES__home_samumoga_robocomp_components_chocachoca_generated.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
