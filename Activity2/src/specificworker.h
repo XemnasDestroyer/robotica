415d35bb2fb0f5afb07505e02d196b91a6111459
XemnasDestroyer <p.casadomorcillo@gmail.com>
2025-10-28 17:37:25 +0100

    Subida de archivos base

--- Activity2/src/specificworker.h
+++ Activity2/src/specificworker.h
@@ -27,31 +27,12 @@
 #ifndef SPECIFICWORKER_H
 #define SPECIFICWORKER_H
 
+
 // If you want to reduce the period automatically due to lack of use, you must uncomment the following line
 //#define HIBERNATION_ENABLED
 
 #include <genericworker.h>
-#include <expected>
-#include <random>
-#include <chrono>
 
-//Dibuja una ventana
-#include <abstract_graphic_viewer/abstract_graphic_viewer.h>
-
-//Para paralelizar filter_isolated_points()
-#ifdef emit
-#undef emit
-#endif
-#include <algorithm>
-#include <execution>
-#include<cppitertools/enumerate.hpp>
-
-enum class State {
-	FORWARD,
-	TURN,
-	FOLLOW_WALL,
-	SPIRAL
-};
 
 /**
  * \brief Class SpecificWorker implements the core functionality of the component.
@@ -86,8 +67,6 @@ public slots:
 	 */
 	void compute();
 
-	void update_robot_position();
-
 	/**
 	 * \brief Handles the emergency state loop.
 	 */
@@ -104,122 +83,15 @@ public slots:
      */
 	int startup_check();
 
-	//graphics
-	void new_target_slot(QPointF p);
-
 private:
 
-	struct Params
-	{
-		float ROBOT_LENGTH = 400;  // mm
-		float MAX_ADV_SPEED = 1000; // mm/s
-        float MAX_ROTATION_SPEED = 1.6f;
-		float FRONT_SECTION = M_PI/6;  // 30°
-		float SIDE_SECTION = M_PI/2; // 90°
-		int rot_direction = 1;
-
-		// Si previamente se ejecutó forward
-		bool previousForward = false;
-
-		// Si previamente se ejecutó followWall
-		bool previousFollowWall = false;
-		// Distancia a la que nos queremos mantener del muro
-		float followWallSafeDistance = 500;
-
-		// Veces que se repite turn
-		bool previousTurn = false;
-
-		// Variables para calcular el tiempo que se ejecutan las acciones
-		std::chrono::steady_clock::time_point action_start_time;
-		// Maximo tiempo que una accion se va a ejecutar
-		std::chrono::milliseconds action_duration;
-
-		std::string LIDAR_NAME_LOW = "bpearl";
-		std::string LIDAR_NAME_HIGH = "helios";
-	};
-	Params params;
-	// Movimiento con el que comienza
-	State current_state = State::SPIRAL;
-
 	/**
-     * @brief Flag indicating whether startup checks are enabled.
+     * \brief Flag indicating whether startup checks are enabled.
      */
 	bool startup_check_flag;
 
-	// graphics
-	QRectF dimensions;
-	AbstractGraphicViewer *viewer;
-	QGraphicsPolygonItem *robot_polygon;
-	std::chrono::steady_clock::time_point last_turn_time;
-
-	// Estos métodos nos lo da pablo ya hechos en el doc
-    void draw_lidar(const RoboCompLidar3D::TPoints& filtered_points, QGraphicsScene *scene);
-    /**
-	* @brief Calculates the index of the closest lidar point to the given angle.
-	*
-	* This method searches through the provided std::list of lidar points and finds the point
-	* whose angle (phi value) is closest to the specified angle. If a matching point is found,
-	* the index of the point in the std::list is returned. If no point is found that matches the condition,
- 	* an error message is returned.
-	*
-	* @param points The collection of lidar points to search through.
-	* @param angle The target angle to find the closest matching point.
-	* @return std::expected<int, std::string> containing the index of the closest lidar point if found,
-	* or an error message if no such point exists.
-	*/
-	std::expected<int, std::string> closest_lidar_index_to_given_angle(const RoboCompLidar3D::TPoints& points, float angle);
-
-	std::optional<RoboCompLidar3D::TPoints> filter_min_distance_cppitertools(const RoboCompLidar3D::TPoints& points);
-
-	// Filter isolated points: keep only points with at least one neighbour within distance d (mm)
-    RoboCompLidar3D::TPoints filter_isolated_points(const RoboCompLidar3D::TPoints &points, float d);
-
-	// Estos métodos ya son nuestros
-	/**
-    * @brief Comportamiento de espiral del robot.
-    * Disminuye gradualmente el ángulo de rotación para abrir la espiral.
-    * Si detecta un obstáculo en el rango [700, 730], cambia a FORWARD.
-    *
-    * @param points Vector de puntos Lidar 3D.
-    * @return std::tuple<State, float, float> Estado siguiente, velocidad de avance y rotación.
-    */
-	std::tuple<State,float,float,float> Spiral(const RoboCompLidar3D::TPoints &points);
-
-    /**
-    * @brief Comportamiento de avance recto del robot.
-    * Verifica si hay obstáculos en el centro del campo de visión.
-    *
-    * @param points Vector de puntos Lidar 3D.
-    * @return std::tuple<State, float, float> Estado siguiente, velocidad de avance y rotación.
-    */
-	std::tuple<State, float, float, float> Forward(const RoboCompLidar3D::TPoints &points);
-
-    /**
-    * @brief Comportamiento de seguimiento de pared.
-    * Busca el punto más cercano y evalúa si hay obstáculos frontales.
-    * Puede cambiar aleatoriamente a TURN o a otros estados según condiciones.
-    *
-    * @param points Vector de puntos Lidar 3D.
-    * @return std::tuple<State, float, float> Estado siguiente, velocidad de avance y rotación.
-    */
-	std::tuple<State, float, float, float> Follow_Wall(const RoboCompLidar3D::TPoints &points);
-
-    /**
-    * @brief Comportamiento de giro en el lugar.
-    * Gira durante un número limitado de ciclos antes de cambiar a espiral.
-    *
-    * @return std::tuple<State, float, float> Estado siguiente, velocidad cero y rotación.
-    */
-	std::tuple<State, float, float, float> Turn(const RoboCompLidar3D::TPoints &points);
-
-	std::expected<RoboCompLidar3D::TPoint, std::string> closest_lidar_point(const RoboCompLidar3D::TPoints &points, bool front, int range);
-
-	void setMinDistanceUI(float distance);
-	void setFrontMinDistanceUI(float distance);
-	void setActionNameUI(std::string text);
 signals:
 	//void customSignal();
 };
 
 #endif
-
